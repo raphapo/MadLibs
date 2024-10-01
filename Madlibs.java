1f411297dc8231baede09341640894c9d4f33515
Raphael <raphael.ponchon27@student.tamdistrict.org>
2024-10-01 09:37:28 -0700

    it works

--- Madlibs.java
+++ Madlibs.java
@@ -4,22 +4,36 @@ public class Madlibs{
      
         Scanner sc = new Scanner(System.in);
        
-        String madlib = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
-        madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
-        madlib = "when <name> was <ingVerb> very fast he found a <noun>.";
+        String madlib1 = "I went to the animal <noun>, the <plural_noun> and the <number> beasts were there.";
 
-        madlib.indexOf("<noun>");
-        madlib.indexOf("<plural noun>");
-        madlib.indexOf("<number>"); //first mad lib
-        madlib.indexOf("<adjective>");
-        madlib.indexOf("<noun>");
-        madlib.indexOf("<adverb>");//second madlib
-        madlib.indexOf("<name>");
-        madlib.indexOf("<ningVerb>");
-        madlib.indexOf("<noun>");//third noun
+        //first word
+        int bracketStart1 = madlib1.indexOf("<");
+        int bracketEnd1 = madlib1.indexOf(">");
+        String sub1 = madlib1.substring(bracketStart1+1, bracketEnd1);
+    
+        System.out.println("Enter a "+ sub1+":");//ask user to enter a [word]
+        String input1 = sc.nextLine();
+        madlib1 = madlib1.replaceFirst("<"+sub1+">",input1);//replace <....> by user word
 
-        System.out.println("Enter a "+ madlib.indexOf+":");
-        String input = sc.nextLine();
+        //second word
+        int bracketStart2 = madlib1.indexOf("<");
+        int bracketEnd2 = madlib1.indexOf(">");
+        String sub2 = madlib1.substring(bracketStart2+1, bracketEnd2);
+    
+        System.out.println("Enter a "+ sub2+":");//ask user to enter a [word]
+        String input2 = sc.nextLine();
+        madlib1 = madlib1.replaceFirst("<"+sub2+">",input2);//replace <....> by user word
+
+        //third word
+        int bracketStart3 = madlib1.indexOf("<");
+        int bracketEnd3 = madlib1.indexOf(">");
+        String sub3 = madlib1.substring(bracketStart3+1, bracketEnd3); 
+    
+        System.out.println("Enter a "+ sub3+":"); //ask user to enter a [word]
+        String input3 = sc.nextLine();
+        madlib1 = madlib1.replaceFirst("<"+sub3+">",input3);//replace <...> by user word
+
+        System.out.println(madlib1);
 
     }
 
